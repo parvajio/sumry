d29d42f0e1532c3555a75948705e7149dcd13462
parvajio <mdparvajmosharof17@gmail.com>
2025-04-17 23:38:10 +0600

    shadcn added

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from "@/components/ui/button";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <Button>Click me</Button>
   );
 }
